87fae83211a236814713f4e4f117c7562b6c0147
Brock Fanning <brockfanning@gmail.com>
2020-01-10 22:16:14 -0500

    Clarify and rename config-related files

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -1,7 +1,7 @@
 from sdg.open_sdg import open_sdg_check
 
 # Validate the indicators.
-validation_successful = open_sdg_check(config='_config.yml')
+validation_successful = open_sdg_check(config='config_data.yml')
 
 # If everything was valid, perform the build.
 if not validation_successful:
